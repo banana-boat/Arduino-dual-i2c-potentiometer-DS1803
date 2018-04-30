5620a44aa0aa67ca22ae7e5069e27b1878c6ca58
Pablo Nieto <pnieto@teltek.es>
2018-04-30 18:57:48 +0200

    lee desde input el valor de los potenciometro

--- Arduino-dual-i2c-potentiometer-DS1803.ino
+++ Arduino-dual-i2c-potentiometer-DS1803.ino
@@ -5,6 +5,7 @@
    0x28 is the address of the DS1803
    with three pin grounded. */
 DS1803 pot(0x28);
+int val = 128;
 
 void setup() {
   //Begin serial connection at 9600 baud
@@ -12,14 +13,37 @@ void setup() {
 }
 
 void loop() {
-  //Set the first potentiometer at 200
-  pot.setPot(3,0);
-  //Set the second potentiometer at 255
-  pot.setPot(4,1);
+  //Serial.println("Valor para pot0 [0-255]: ");
+  
+  while(Serial.available() == 0){
+    ;
+  }
+  
+  val = Serial.parseInt(); //Leer un entero por serial
+  if ((val > 0) and (val < 256))
+  {
+	  //Set the first potentiometer at 200
+  	pot.setPot(val,0);
+    Serial.print("Valor para pot0: ");
+	  Serial.println(val);
+	}
 
-  //Read the values saved on the DS1803
-  int8_t *value=pot.getValue();
-  Serial.println(value[0],DEC);
-  Serial.println(value[1],DEC);
+  while(Serial.available() == 0) {
+    ;
+  }
+  
+  val = Serial.parseInt(); //Leer un entero por serial
+  if ((val > 0) and (val < 256))
+  {
+	  //Set the first potentiometer at 200
+    Serial.print("Valor para pot1: ");
+	  pot.setPot(val,1);
+	  Serial.println(val);
+	}
+  	//Read the values saved on the DS1803
+  	int8_t *value=pot.getValue();
+  	Serial.println(value[0],DEC);
+  	Serial.println(value[1],DEC);
 }
+
 
